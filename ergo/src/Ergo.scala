b56668ad08681023f20ef554c89becf89066ea78
jonas Anso <jonas.anso@gmail.com>
2020-04-29 14:50:30 +0100

    Added summary implementation

--- ergo/src/Ergo.scala
+++ ergo/src/Ergo.scala
@@ -1,13 +1,45 @@
 import scala.util.Random
-import org.rogach.scallop.{ScallopOption, _}
-
+import org.rogach.scallop._
+import BasicMath._
 object Ergo {
 
   def main(args: Array[String]): Unit = {
+    val conf = new Conf(args)
+    val agents: LazyList[Agents] = Agents.fromConf(conf)
+    if (conf.games.isDefined)
+      runSummary(agents)
+    else
+      runGraphics(agents)
+  }
+
+  private def runSummary(agents: LazyList[Agents]): Unit = {
+    val init = agents.head
+    val originalWealth = init.max
+    val end = agents.last
+
+    val percentageAlive = percentage(end.count, init.count)
+    val percentageNetWinners = percentage(end.wealthAbove(originalWealth).length, init.count)
+
+    val formatter = java.text.NumberFormat.getCurrencyInstance
+    def format(d: BigDecimal): String = formatter.format(d)
+    
+    println(
+      s"""Initial wealth ${format(init.sum)} ${init.count} agents with ${format(init.max)} each
+          |Survivors      $percentageAlive%
+          |Winners        $percentageNetWinners%
+          |Poorest wealth ${format(end.min)}
+          |Average wealth ${format(end.mean)}
+          |Median  wealth ${format(end.median)}
+          |Richest wealth ${format(end.max)} or ${percentage(end.max, end.sum)}% of total
+          |Final wealth   ${format(end.sum)}
+          |Total games    ${agents.length}""".stripMargin)
+  }
+
+  private def runGraphics(agents: LazyList[Agents]): Unit = {
     for {
-      agents <- Agents.fromConf(new Conf(args))
+      a <- agents
     } {
-      println(agents.toJson)
+      println(a.toJson)
       Thread.sleep(100)
     }
 
@@ -25,76 +57,88 @@ class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
   val mode: ScallopOption[String] = choice(Seq(ADDITIVE, MULTIPLICATIVE), name = "mode", short = 'm', descr = "Mode of execution, `" + ADDITIVE + "` will add or subtract total amounts while `" + MULTIPLICATIVE + "` will add or subtract a percentage of the agent wealth", default = Some(ADDITIVE))
   val win: ScallopOption[Int] = opt[Int](name = "win", short = 'w', descr = "Amount to increase in case of wining", default = Some(50))
   val lose: ScallopOption[Int] = opt[Int](name = "lose", short = 'l', descr = "Amount to increase in case of loosing", default = Some(40))
+
   verify()
 }
 
-class Agents(values: List[BigDecimal], fn: (BigDecimal, Boolean) => BigDecimal) {
+class Agents(values: Vector[BigDecimal], fn: (BigDecimal, Boolean) => BigDecimal, next: () => Boolean = Random.nextBoolean) {
 
   import AgentMath._
 
-  private val alive: List[BigDecimal] = values.filter(_ > Agents.minWealth)
+  private val alive: Vector[BigDecimal] = wealthAbove(Agents.minWealth)
+
+  def wealthAbove(min: BigDecimal): Vector[BigDecimal] = values.filter(_ >= min)
 
   val count: Int = alive.length
 
   def lazylist: LazyList[Agents] = {
     if (count == 0) this #:: LazyList.empty
-    else this #:: new Agents(values.map(nextValue), fn).lazylist
+    else this #:: new Agents(values.map(nextValue), fn, next).lazylist
   }
 
   private def nextValue(v: BigDecimal): BigDecimal = {
-    if (v > Agents.minWealth)
-      round(fn(v, Random.nextBoolean)).max(Agents.minWealth)
-    else v
+    if (v < Agents.minWealth) v
+    else {
+      val res = round(fn(v, next())).max(0d)
+      if (res < Agents.minWealth) 0 else res
+    }
   }
 
-  private val max: BigDecimal = values.max
+  val max: BigDecimal = values.max
 
-  private val min: BigDecimal = alive match {
-    case Nil => 0
-    case l => l.min
-  }
+  val min: BigDecimal = alive.minOption.getOrElse(0)
 
-  private val sum: BigDecimal = values.sum
+  val sum: BigDecimal = values.sum
 
-  private val mean: BigDecimal = sum / values.length
+  val mean: BigDecimal = sum / values.length
 
-  private def median: BigDecimal = getMedian(values)
+  def median: BigDecimal = getMedian(values)
 
   val toJson: String =
     s"""{"agents":$count,""" +
       s""""wealth":{"max":$max,"min":$min,"mean":$mean,"median":$median,"sum":$sum,""" +
-      allPercentiles(values).map { case (k, v) => s""""p$k":$v""" }.mkString (",") +
+      allPercentiles(values).map { case (k, v) => s""""p$k":$v""" }.mkString(",") +
       """}}"""
 }
 
 
 object Agents {
-  val minWealth = 0
+  val minWealth = 1
 
   def fromConf(conf: Conf): LazyList[Agents] = {
     val fn = if (conf.mode() == conf.MULTIPLICATIVE) multiply(conf.win(), conf.lose()) _ else add(conf.win(), conf.lose()) _
-    val lazylist = new Agents(List.fill(conf.numAgents())(conf.initialAgentWealth()), fn).lazylist
+    val lazylist = new Agents(Vector.fill(conf.numAgents())(conf.initialAgentWealth()), fn).lazylist
     conf.games.toOption match {
       case Some(g) => lazylist.take(g)
       case None => lazylist
     }
   }
 
-  private def add(plus: BigDecimal, minus: BigDecimal)(wealth: BigDecimal, win: Boolean): BigDecimal =
+  def add(plus: BigDecimal, minus: BigDecimal)(wealth: BigDecimal, win: Boolean): BigDecimal =
     if (win) wealth + plus else wealth - minus
 
-  private def multiply(plus: Int, minus: Int)(wealth: BigDecimal, wih: Boolean): BigDecimal =
-    if (wih) wealth + (wealth * plus / 100) else wealth - (wealth * minus / 100)
+  def multiply(plus: Int, minus: Int)(wealth: BigDecimal, wih: Boolean): BigDecimal =
+    if (wih) wealth + (wealth * plus / 100) else wealth - (wealth * minus / 100).max(minWealth)
+
+}
+
+object BasicMath {
+
+  def percentage(a: Int, b: Int): BigDecimal = percentage(BigDecimal(a), BigDecimal(b))
+  def percentage(a: BigDecimal, b: BigDecimal): BigDecimal = if (b == 0) 0d else round(a * 100 / b)
+
+  def round(v: BigDecimal): BigDecimal = BigDecimal(truncate(v.toDouble))
+
+  private def truncate(n: Double): Double = {
+    math.floor(n * 100) / 100 match {
+      case Double.PositiveInfinity => Double.MaxValue
+      case x => x
+    }
+  }
 
 }
 
 object AgentMath {
-  def round(v: BigDecimal): BigDecimal = BigDecimal(truncate(v.toDouble))
-
-  private def truncate(n: Double): Double = {
-    math.floor(n * 100) / 100
-  }
-
   def getMedian[T: Ordering, F](seq: Seq[T])(implicit conv: T => F, f: Fractional[F]): F = {
     val sortedSeq = seq.sorted
     if (seq.size % 2 == 1) sortedSeq(sortedSeq.size / 2) else {
@@ -117,7 +161,7 @@ object AgentMath {
       mille -> sorted(k)
     }
 
-    List(
+    Vector(
       percentile(10) _,
       percentile(20) _,
       percentile(30) _,
